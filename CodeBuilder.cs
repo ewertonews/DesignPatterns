38a5549d6c640172b41da7478ab1126b9fff84a5
Ewerton Willams <ewertonews@hotmail.com>
2020-09-12 22:02:06 -0300

    Add project files.

---
+++ CodeBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public class FieldElement
+    {
+        public string ClassName;
+        public string Field, Type;
+        public List<FieldElement> Fields = new List<FieldElement>();
+        private const int indentSize = 2;
+
+        public FieldElement(string field, string type)
+        {
+            Field = field;
+            Type = type;
+        }
+
+        public FieldElement()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize);
+            sb.Append($"public class {ClassName}\n{{\n");
+            foreach (var field in Fields)
+            {
+                sb.Append($"{i}public {field.Type} {field.Field};");
+                sb.Append("\n");
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+    }
+    public class CodeBuilder
+    {
+        FieldElement _class = new FieldElement();
+        public CodeBuilder(string className)
+        {
+            _class.ClassName = className;
+        }
+
+        public CodeBuilder()
+        {
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            _class.Fields.Add(new FieldElement
+            {
+                Field = name,
+                Type = type
+            });
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _class.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        public enum Color
+        {
+            Red, Green, Blue
+        }
+
+        public enum Size
+        {
+            Small, Medium, Large, Huge
+        }
+
+        public class Product
+        {
+            public string Name;
+            public Color Color;
+            public Size Size;
+
+            public Product()
+            {
+                    
+            }
+        }
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person");
+            cb.AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb.ToString());
+        }
+    }
+}
